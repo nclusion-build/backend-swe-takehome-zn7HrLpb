6eaca35ba431df40efc72cae5710913e714a5db6
Van Nguyen <thegoleffect@gmail.com>
2025-08-21 13:45:46 -0700

    Add src/services.py

---
+++ src/services.py
@@ -0,0 +1,5 @@
+# TODO: Implement service functions to orchestrate model operations
+# e.g., create_game, get_game_by_id, join_game, make_move, get_status, list_games, delete_game, get_stats
+
+
+// TODO: Implement services in Python to orchestrate models [ttt.todo.py.service.complete]

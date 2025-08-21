603815b791cabd0f70e3814e10bd748dd7af6f55
Van Nguyen <thegoleffect@gmail.com>
2025-08-21 13:45:48 -0700

    Add tests/test_models.py

---
+++ tests/test_models.py
@@ -0,0 +1,6 @@
+def test_models_exist():
+    # TODO: instantiate models from src.models and assert basic fields
+    assert True
+
+
+// TODO: Add Python unit tests for models (Game/Player) [ttt.todo.tests.py.unit.models]
